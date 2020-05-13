e5cbfece5d377239df546fc020838ec4ebf8a8fb
MARS_Penguine <lbit123@outlook.com>
2020-05-13 21:10:36 +1000

    Add alert and put userid and password in header

--- studysys/src/Pages/login/login.js
+++ studysys/src/Pages/login/login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, Icon, Input, Button, Row, Col, Divider } from "antd";
 import { fakeAuth } from "../route/privateRoute";
+import Axios from "axios";
 
 function hasErrors(fieldsError) {
   return Object.keys(fieldsError).some((field) => fieldsError[field]);
@@ -9,15 +10,34 @@ function hasErrors(fieldsError) {
 //Login page
 function Login(props) {
   const { getFieldDecorator } = props.form;
-  console.log(props.location.state.from);
+  // Verify user ID and password
+  const verifyUser = (values) => {
+    // let postData = JSON.stringify({
+    //   user_id: values.username,
+    //   password: values.password,
+    // });
+    let axiosConfig = {
+      "Content-Type": "application/json",
+      user_id: values.username,
+      password: values.password,
+    };
+    Axios.post("http://localhost:8080", {}, { headers: axiosConfig }).then(
+      (res) => {
+        console.log(res);
+      }
+    );
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     props.form.validateFields((err, values) => {
       if (!err) {
         console.log("Received values of form: ", values);
-        console.log(props.location.state.from);
-        props.history.push(props.location.state.from);
+        verifyUser(values);
+        // if (verifyUser(values)) {
+        // }
+        props.form.props.history.push(props.location.state.from);
       }
     });
   };
