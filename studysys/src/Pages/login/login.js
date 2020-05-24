89e75c279da10f1b5ee03d7b17864d1965f48f1c
MARS_Penguine <lbit123@outlook.com>
2020-05-24 20:28:37 +1000

    Update UseEffect

--- studysys/src/Pages/login/login.js
+++ studysys/src/Pages/login/login.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Form, Icon, Input, Button, Row, Col, Divider } from "antd";
 import Axios from "axios";
 import AlertMessage from "./alertMessage";
+import { useHistory } from "react-router-dom";
 
 function hasErrors(fieldsError) {
   return Object.keys(fieldsError).some((field) => fieldsError[field]);
@@ -12,88 +13,88 @@ function Login(props) {
   const { getFieldDecorator } = props.form;
   const [isAuthenticated, setIsAuthenticated] = useState(undefined);
   const [message, setMessage] = useState(undefined);
+  let history = useHistory();
+
   const [token, setToken] = useState(
     localStorage.getItem("token") || undefined
   );
-  const [userId, setUserId] = useState(undefined);
-  const [password, setPassword] = useState(undefined);
   const BASE_URL = "http://localhost:8080/token";
 
-  useEffect(() => {
-    // Verify user authentication then fetch a token from backend server
-    const verifyUser = async () => {
-      // Headers config
-      let headersConfig = {
-        "Content-Type": "application/json",
-        user_id: userId,
-        password: password,
-      };
+  // Store new token in Local Storage
+  const saveToken = async (token) => {
+    console.log("save token to LocalStorage, token: " + token);
+    localStorage.setItem("token", token);
+    setToken(token);
+  };
 
-      // Send a POST request with userId and password
-      let response = undefined;
-      try {
-        response = await Axios.post(BASE_URL, {}, { headers: headersConfig });
-      } catch (e) {
-        response = e.response;
-      } finally {
-        setIsAuthenticated(
-          response === undefined ? false : response.status === 200
-        );
-
-        setMessage(
-          response === undefined
-            ? "Server is down, please try again later"
-            : response.data.Message
-        );
-
-        setToken(
-          response === undefined
-            ? undefined
-            : response.data.Token === undefined
-            ? undefined
-            : response.data.Token
-        );
-      }
+  // Verify user authentication then fetch a token from backend server
+  const verifyUser = async (userId, password) => {
+    // Headers config
+    let headersConfig = {
+      "Content-Type": "application/json",
+      user_id: userId,
+      password: password,
     };
 
-    // Avoid it automatically sends a request after render
-    if (password !== undefined) verifyUser();
-  }, [password, userId]);
+    // Send a POST request with userId and password
+    let response = undefined;
+    try {
+      response = await Axios.post(BASE_URL, {}, { headers: headersConfig });
+    } catch (e) {
+      response = e.response;
+    } finally {
+      setIsAuthenticated(
+        response === undefined ? false : response.status === 200
+      );
 
-  // Store new token in Local Storage
-  useEffect(() => {
-    if (token !== undefined) {
-      localStorage.setItem("token", token);
+      let message, token, isAuthenticated;
+      message =
+        response === undefined
+          ? "Server is down, please try again later"
+          : response.data.Message;
+      token =
+        response === undefined
+          ? undefined
+          : response.data.Token === undefined
+          ? undefined
+          : response.data.Token;
+      isAuthenticated =
+        response === undefined ? false : response.status === 200;
+
+      return { message, token, isAuthenticated };
     }
-  }, [token]);
+  };
 
-  const handleSubmit = (e) => {
+  function handleSubmit(e) {
     e.preventDefault();
     props.form.validateFields((err, values) => {
       if (!err) {
-        setUserId(values.username);
-        setPassword(values.password);
+        verifyUser(values.username, values.password).then((data) => {
+          if (data.isAuthenticated) {
+            let path = props.location.state.from;
+            // console.log(path);
+            saveToken(data.token).then(() => {
+              console.log("redirect to /home");
+              history.push({
+                pathname: path,
+              });
+              setMessage(data.message);
+              setIsAuthenticated(data.isAuthenticated);
+            });
+          } else {
+            setMessage(data.message);
+            setIsAuthenticated(data.isAuthenticated);
+          }
+        });
       }
     });
-  };
+  }
 
   return (
     <div align="middle" style={{ height: "100%" }}>
       {isAuthenticated !== undefined ? (
         <AlertMessage isAuthenticated={isAuthenticated} message={message} />
       ) : null}
-
-      {/*<p>*/}
-      {/*  {userId +*/}
-      {/*    " " +*/}
-      {/*    password +*/}
-      {/*    " " +*/}
-      {/*    isAuthenticated +*/}
-      {/*    " " +*/}
-      {/*    message +*/}
-      {/*    " " +*/}
-      {/*    token}*/}
-      {/*</p>*/}
 
       <Row
         type="flex"
@@ -110,11 +111,7 @@ function Login(props) {
               height: "1.5px",
             }}
           />
-          <Form
-            onSubmit={handleSubmit}
-            className="login-form"
-            style={{ marginBottom: "20px" }}
-          >
+          <Form className="login-form" style={{ marginBottom: "20px" }}>
             <Form.Item>
               {getFieldDecorator("username", {
                 rules: [{ required: true, message: "Enter your userName" }],
@@ -152,9 +149,7 @@ function Login(props) {
                 type="primary"
                 htmlType="submit"
                 className="login-form-button"
-                // onClick={() => {
-                //   handleSubmit();
-                // }}
+                onClick={handleSubmit}
               >
                 Login
               </Button>
--- studysys/src/Pages/route/privateRoute.js
+++ studysys/src/Pages/route/privateRoute.js
@@ -1,35 +1,47 @@
 import React, { useEffect, useState } from "react";
 import { Redirect, Route } from "react-router-dom";
 import Axios from "axios";
+import userEvent from "@testing-library/user-event";
 
 function PrivateRoute({ children, ...rest }) {
-  const [token] = useState(localStorage.getItem("token") || undefined);
-  const [isAuthenticated, setIsAuthenticated] = useState(undefined);
+  let isAuthenticated = false;
+  const verifyUser = async (token) => {
+    // const token = localStorage.getItem("token") || undefined;
+    const BASE_URL = "http://localhost:8080/token";
+    const headersConfig = {
+      "Content-Type": "application/json",
+      Token: token,
+    };
+    // Send a POST request for verify token
+    try {
+      await Axios.post(BASE_URL, {}, { headers: headersConfig });
+      console.log("have a successful token in local");
+      isAuthenticated = true;
+      // setIsAuth(true);
+      return true;
+    } catch (e) {
+      console.log("have a failure token in local");
+      isAuthenticated = false;
+      // setIsAuth(false);
+      return false;
+    }
+  };
+
+  const [isAuth, setIsAuth] = useState(false);
+  const [token] = useState(localStorage.getItem("token"));
 
   useEffect(() => {
-    // Verify token in local storage
-    async function authenticate() {
-      const BASE_URL = "http://localhost:8080/token";
-      const headersConfig = {
-        "Content-Type": "application/json",
-        Token: token,
-      };
-      // Send a POST request for verify token
-      try {
-        await Axios.post(BASE_URL, {}, { headers: headersConfig });
-        setIsAuthenticated(true);
-      } catch (e) {
-        setIsAuthenticated(false);
-      }
-    }
-    authenticate(token);
-  }, [token]);
+    console.log("verify user in private Router");
+    verifyUser(token).then(() => {
+      console.log("isAuthenticated: " + isAuthenticated);
+    });
+  });
 
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        isAuthenticated === undefined ? null : isAuthenticated ? (
+        isAuthenticated ? (
           children
         ) : (
           <Redirect
