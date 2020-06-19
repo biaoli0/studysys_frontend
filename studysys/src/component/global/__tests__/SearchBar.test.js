8aaf8c4a19c66e892e54b0f762d5011da1d68e45
MARS_Penguine <lbit123@outlook.com>
2020-06-19 15:46:05 +1000

    Add test

--- studysys/src/component/global/__tests__/SearchBar.test.js
+++ studysys/src/component/global/__tests__/SearchBar.test.js
@@ -1,7 +1,27 @@
 import { DisplayDataContext } from "../../../config/global/ContextConfig";
 import SearchBar from "../SearchBar";
-import React from "react";
+import React, { useState } from "react";
+import { render } from "@testing-library/react";
+import  renderer  from "react-test-renderer";
+import { Log } from "../../../library/Log";
 
-<DisplayDataContext.Provider value={{ setDisplayData, originData }}>
-  <SearchBar />
-</DisplayDataContext.Provider>;
+
+test("Task should render", () => {
+    // const [displayData, setDisplayData] = useState(null);
+    //   const [originData, setOriginData ] = useState(null);
+
+    let originData = null;
+
+    let displayData = null;
+    const setDisplayData = (newData)=>{
+        displayData = newData;
+    };
+
+    const { getByText } = render(
+    <DisplayDataContext.Provider value={{ setDisplayData, originData }}>
+      <SearchBar />
+    </DisplayDataContext.Provider>
+  );
+
+  Log.print(getByText);
+});
