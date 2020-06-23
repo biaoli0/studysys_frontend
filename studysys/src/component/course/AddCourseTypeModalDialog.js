a5a717d8c659a318a34549d7c279a231b786822f
MARS_Penguine <lbit123@outlook.com>
2020-06-23 17:38:02 +1000

    Update AddCourseTypeModalDialog.js

--- studysys/src/component/course/AddCourseTypeModalDialog.js
+++ studysys/src/component/course/AddCourseTypeModalDialog.js
@@ -1,36 +1,43 @@
-import React, {useState} from 'react';
-import {Button, Input, Modal} from "antd";
+import React, { useState } from "react";
+import { Button, Input, Modal } from "antd";
 
 function AddCourseTypeModalDialog(props) {
-    const [visible, setVisible] = useState(false);
-    const [confirmLoading, setConfirmLoading] = useState(false);
-    const [newTypeName,setNewTypeName] = useState(null);
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [newTypeName, setNewTypeName] = useState(null);
 
-    const onSave = ()=>{
-        setConfirmLoading(true);
-    }
+  const onSave = () => {
+    setConfirmLoading(true);
+  };
 
-    const onChange = (e)=>{
-        setNewTypeName(e.target);
-    }
+  const onChange = (e) => {
+    setNewTypeName(e.target);
+  };
 
-    return (
-        <div>
-            <Button type="primary" onClick={()=>{setVisible(true)}}>
-                Add Type
-            </Button>
+  return (
+    <div>
+      <Button
+        type="primary"
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        Add Type
+      </Button>
 
-        <Modal
-            title="New course type"
-            visible={visible}
-            onOk={onSave}
-            confirmLoading={confirmLoading}
-            onCancel={()=>{setVisible(false)}}
-        >
-            <Input onChange={onChange} />
-        </Modal>
-        </div>
-    );
+      <Modal
+        title="New course type"
+        visible={visible}
+        onOk={onSave}
+        confirmLoading={confirmLoading}
+        onCancel={() => {
+          setVisible(false);
+        }}
+      >
+        <Input onChange={onChange} />
+      </Modal>
+    </div>
+  );
 }
 
 export default AddCourseTypeModalDialog;
